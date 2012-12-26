1c3f2f5e5d2af71e6100195fbf6cf70f23b4958d
Andrew Ek <andrewek@gmail.com>
2012-12-26 11:07:45 -0600

    Initial commit of LTAB scorekeeper app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LtabScorekeeper::Application.config.secret_token = '1f2184f1d7189ba88292bb55e0f0491437ede0b69614e1a95bf8dc58dd72e85512f76044d67b7ceee062e3232b556a89af6c5cc1c07a1b0e790b86963999cc29'
